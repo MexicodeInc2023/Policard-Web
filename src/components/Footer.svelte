c933e8b633cf1b9072228c762dff7f52be2d5fb5
JoseArjona <j.angel.matheis@gmail.com>
2023-07-15 17:23:08 -0600

    Components

---
+++ src/components/Footer.svelte
@@ -0,0 +1,29 @@
+<script>
+	import logo from '../lib/img/policard-wt-sf.png';
+	import logoMex from '../lib/img/mexicode.png';
+</script>
+
+<div class="blockcode">
+	<footer class="shadow" style="background-color: #a4c3b2;">
+		<div
+			class="d-flex justify-content-between align-items-center mx-auto py-4 flex-wrap"
+			style="width: 80%"
+		>
+			<div href="/#" class="d-flex align-items-center p-0 text-dark">
+				<img alt="logo" src={logo} width="30px" loading="lazy" decoding="async" />
+				<span class="ml-4 h5 font-weight-bold">Policard</span>
+			</div>
+			<small
+				>&copy; Universidad Politécnica de Tapachula, By Mexic0de. Todos los derechos reservados.</small
+			>
+			<div href="/#" class="d-flex align-items-center p-0 text-dark">
+				<img alt="logo" src={logoMex} width="30px" loading="lazy" decoding="async" />
+				<span class="ml-4 h5 font-weight-bold">Mexic0de</span>
+			</div>
+		</div>
+	</footer>
+</div>
+
+<style>
+	footer{background-color:#e4e4e4;padding:10px 0;width:100%;height:auto;color:#000}@media print{footer{display:none}}
+</style>
---
+++ src/components/Header.svelte
@@ -0,0 +1,123 @@
+<script>
+	import { page } from '$app/stores';
+	import logo from '../lib/img/policard-wt-sf.png';
+	import { goto } from '$app/navigation';
+
+	export let loginData;
+	/* import {
+		customerEmail,
+		customerName,
+	} from '../routes/form/components/sharedState';
+	
+	import {
+		customerDate
+	} from '../routes/form/components/sharedState' */
+	/* const logoutUrl = 'https://policard-api.onrender.com/api/logout/';
+	let auth = false;
+	authenticated.subscribe((value) => (auth = value));
+	 */
+
+	async function handleSignOut(event) {
+		await goto(`/?signout=${true}`, { replaceState: true });
+		location.reload();
+	}
+	/* let name = customerName.name;
+	
+	let correo = customerEmail;
+	let fecha = customerDate; */
+</script>
+
+<header>
+	<nav class="navbar navbar-expand-lg start-header">
+		<div class="container-fluid">
+			<a class="navbar-brand" href="/">
+				<img src={logo} alt="" width="50" height="50" class="d-inline-block align-text-top" loading="lazy" decoding="async" />
+			</a>
+
+			<button
+				id="Modal"
+				class="navbar-toggler"
+				type="button"
+				data-bs-toggle="collapse"
+				data-bs-target="#navbarColor03"
+				aria-controls="navbarColor03"
+				aria-expanded="false"
+				aria-label="Toggle navigation"
+			>
+				<span class="navbar-toggler-icon" />
+			</button>
+			<div class="collapse navbar-collapse" id="navbarColor03">
+				<ul class="navbar-nav me-auto">
+					<div class="logo">POLICARD</div>
+					<li
+						class={$page.url.pathname === '/credentials' ? 'nav-item active' : 'nav-item '}
+						aria-current={$page.url.pathname === '/credentials' ? 'page' : undefined}
+					>
+						<a class="nav-link" href="/credentials">Inicio</a>
+					</li>
+					<li
+						class={$page.url.pathname === '/credentials/card' ? 'nav-item active' : 'nav-item '}
+						aria-current={$page.url.pathname === '/credentials/card' ? 'page' : undefined}
+					>
+						<a class="nav-link" href="/credentials/card">Mi Credencial</a>
+					</li>
+					<li
+						class={$page.url.pathname === '/credentials/procedures'
+							? 'nav-item active'
+							: 'nav-item '}
+						aria-current={$page.url.pathname === '/credentials/procedures' ? 'page' : undefined}
+					>
+						<a class="nav-link" href="/credentials/procedures">Tramites</a>
+					</li>
+					<li
+						class={$page.url.pathname === '/credentials/info' ? 'nav-item active' : 'nav-item '}
+						aria-current={$page.url.pathname === '/credentials/procedures' ? 'page' : undefined}
+					>
+						<a class="nav-link" href="/credentials/info">Informacion</a>
+					</li>
+
+					<li class="nav-item dropdown d-flex">
+						<a
+							class="nav-link dropdown-toggle"
+							data-bs-toggle="dropdown"
+							href="#"
+							role="button"
+							aria-haspopup="true"
+							aria-expanded="false"
+						>
+							Perfil
+						</a>
+						<!-- Modal -->
+						<div class="modal fade" id="myModal" role="dialog">
+							<div class="modal-dialog">
+								<div class="card">
+									<div class="card-img">
+										<img class="img-fluid" alt="" src={logo} loading="lazy" decoding="async" />
+									</div>
+									<div class="card-title">
+										<p>{loginData.user}</p>
+									</div>
+									<div class="card-text">
+										<p>{loginData.emailUser}</p>
+									</div>
+									<button class="btn">Cerrar</button>
+								</div>
+							</div>
+						</div>
+						<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+							<button class="dropdown-item" data-bs-toggle="modal" data-bs-target={'#myModal'}
+								>Perfil</button
+							>
+							<div class="dropdown-divider" />
+							<button class="dropdown-item" on:click={handleSignOut}>Cerrar Sesion</button>
+						</div>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+</header>
+
+<style>
+	.logo{font-size:2rem;font-weight:700;color:#000;margin-right:1rem}.start-header{opacity:1;padding:15px 0;box-shadow:0 10px 30px 0 rgba(138,155,165,0.15);-webkit-transition:all .3s ease-out;transition:all .3s ease-out}li{margin-right:1rem}a{color:#000;margin-top:.25rem;font-size:1.1rem}.nav-link{color:#212121!important;font-weight:500;transition:all 200ms linear}.nav-item:hover .nav-link{color:#8167a9!important}.nav-item.active .nav-link{color:#249434!important}.nav-link{position:relative;padding:5px 0!important;display:inline-block}.nav-item:after{position:relative;bottom:-5px;left:0;width:100%;height:2px;content:'';background-color:#8167a9;opacity:0;transition:all 200ms linear}.nav-item:hover:after{bottom:0;opacity:1}.nav-item.active:hover:after{opacity:0}.nav-item{position:relative;transition:all 200ms linear}.btn{background-color:#23c483;border-color:#23c483;border-radius:6px;color:#fff;font-size:17px;padding-right:76px;padding-left:76px}.card{border-radius:3vh;margin:auto;max-width:380px;padding:7vh 6vh;align-items:center;box-shadow:0 6px 20px 0 rgba(0,0,0,0.19)}@media (max-width: 767px){.card{width:90vw}}.card-img{padding:20px 0;width:40%}.card-img img{opacity:.7}.card-title{margin-bottom:unset}.card-title p{color:#6c63ff;font-weight:900;font-size:30px;margin-bottom:unset}.card-text p{color:grey;font-size:25px;text-align:center;padding:3vh 0;font-weight:lighter}@media print{header{display:none}}
+</style>
