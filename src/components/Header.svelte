9bf8b4b9df14584c6cf080d43bf2a29afa95507c
JoseArjona <j.angel.matheis@gmail.com>
2023-07-15 21:20:41 -0600

    delete fake logout

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -4,34 +4,26 @@
 	import { goto } from '$app/navigation';
 
 	export let loginData;
-	/* import {
-		customerEmail,
-		customerName,
-	} from '../routes/form/components/sharedState';
-	
-	import {
-		customerDate
-	} from '../routes/form/components/sharedState' */
-	/* const logoutUrl = 'https://policard-api.onrender.com/api/logout/';
-	let auth = false;
-	authenticated.subscribe((value) => (auth = value));
-	 */
 
 	async function handleSignOut(event) {
 		await goto(`/?signout=${true}`, { replaceState: true });
 		location.reload();
 	}
-	/* let name = customerName.name;
-	
-	let correo = customerEmail;
-	let fecha = customerDate; */
 </script>
 
 <header>
 	<nav class="navbar navbar-expand-lg start-header">
 		<div class="container-fluid">
 			<a class="navbar-brand" href="/">
-				<img src={logo} alt="" width="50" height="50" class="d-inline-block align-text-top" loading="lazy" decoding="async" />
+				<img
+					src={logo}
+					alt=""
+					width="50"
+					height="50"
+					class="d-inline-block align-text-top"
+					loading="lazy"
+					decoding="async"
+				/>
 			</a>
 
 			<button
@@ -77,16 +69,14 @@
 					</li>
 
 					<li class="nav-item dropdown d-flex">
-						<a
+						<button
 							class="nav-link dropdown-toggle"
 							data-bs-toggle="dropdown"
-							href="#"
-							role="button"
 							aria-haspopup="true"
 							aria-expanded="false"
 						>
 							Perfil
-						</a>
+						</button>
 						<!-- Modal -->
 						<div class="modal fade" id="myModal" role="dialog">
 							<div class="modal-dialog">
@@ -119,5 +109,113 @@
 </header>
 
 <style>
-	.logo{font-size:2rem;font-weight:700;color:#000;margin-right:1rem}.start-header{opacity:1;padding:15px 0;box-shadow:0 10px 30px 0 rgba(138,155,165,0.15);-webkit-transition:all .3s ease-out;transition:all .3s ease-out}li{margin-right:1rem}a{color:#000;margin-top:.25rem;font-size:1.1rem}.nav-link{color:#212121!important;font-weight:500;transition:all 200ms linear}.nav-item:hover .nav-link{color:#8167a9!important}.nav-item.active .nav-link{color:#249434!important}.nav-link{position:relative;padding:5px 0!important;display:inline-block}.nav-item:after{position:relative;bottom:-5px;left:0;width:100%;height:2px;content:'';background-color:#8167a9;opacity:0;transition:all 200ms linear}.nav-item:hover:after{bottom:0;opacity:1}.nav-item.active:hover:after{opacity:0}.nav-item{position:relative;transition:all 200ms linear}.btn{background-color:#23c483;border-color:#23c483;border-radius:6px;color:#fff;font-size:17px;padding-right:76px;padding-left:76px}.card{border-radius:3vh;margin:auto;max-width:380px;padding:7vh 6vh;align-items:center;box-shadow:0 6px 20px 0 rgba(0,0,0,0.19)}@media (max-width: 767px){.card{width:90vw}}.card-img{padding:20px 0;width:40%}.card-img img{opacity:.7}.card-title{margin-bottom:unset}.card-title p{color:#6c63ff;font-weight:900;font-size:30px;margin-bottom:unset}.card-text p{color:grey;font-size:25px;text-align:center;padding:3vh 0;font-weight:lighter}@media print{header{display:none}}
+	.logo {
+		font-size: 2rem;
+		font-weight: 700;
+		color: #000;
+		margin-right: 1rem;
+	}
+	.start-header {
+		opacity: 1;
+		padding: 15px 0;
+		box-shadow: 0 10px 30px 0 rgba(138, 155, 165, 0.15);
+		-webkit-transition: all 0.3s ease-out;
+		transition: all 0.3s ease-out;
+	}
+	li {
+		margin-right: 1rem;
+	}
+	a {
+		color: #000;
+		margin-top: 0.25rem;
+		font-size: 1.1rem;
+	}
+	.nav-link {
+		color: #212121 !important;
+		font-weight: 500;
+		transition: all 200ms linear;
+	}
+	.nav-item:hover .nav-link {
+		color: #8167a9 !important;
+	}
+	.nav-item.active .nav-link {
+		color: #249434 !important;
+	}
+	.nav-link {
+		position: relative;
+		padding: 5px 0 !important;
+		display: inline-block;
+	}
+	.nav-item:after {
+		position: relative;
+		bottom: -5px;
+		left: 0;
+		width: 100%;
+		height: 2px;
+		content: '';
+		background-color: #8167a9;
+		opacity: 0;
+		transition: all 200ms linear;
+	}
+	.nav-item:hover:after {
+		bottom: 0;
+		opacity: 1;
+	}
+	.nav-item.active:hover:after {
+		opacity: 0;
+	}
+	.nav-item {
+		position: relative;
+		transition: all 200ms linear;
+	}
+	.btn {
+		background-color: #23c483;
+		border-color: #23c483;
+		border-radius: 6px;
+		color: #fff;
+		font-size: 17px;
+		padding-right: 76px;
+		padding-left: 76px;
+	}
+	.card {
+		border-radius: 3vh;
+		margin: auto;
+		max-width: 380px;
+		padding: 7vh 6vh;
+		align-items: center;
+		box-shadow: 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+	}
+	@media (max-width: 767px) {
+		.card {
+			width: 90vw;
+		}
+	}
+	.card-img {
+		padding: 20px 0;
+		width: 40%;
+	}
+	.card-img img {
+		opacity: 0.7;
+	}
+	.card-title {
+		margin-bottom: unset;
+	}
+	.card-title p {
+		color: #6c63ff;
+		font-weight: 900;
+		font-size: 30px;
+		margin-bottom: unset;
+	}
+	.card-text p {
+		color: grey;
+		font-size: 25px;
+		text-align: center;
+		padding: 3vh 0;
+		font-weight: lighter;
+	}
+	@media print {
+		header {
+			display: none;
+		}
+	}
 </style>
