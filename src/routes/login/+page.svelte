c72f74e7367f725a82c90d85a4dc969c97b3e5a0
JoseArjona <j.angel.matheis@gmail.com>
2023-07-15 18:38:15 -0600

    detalles

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -53,36 +53,22 @@
 				<!--------------------------- Left Box ----------------------------->
 
 				<div
-					class="col-md-6 rounded-4 d-flex justify-content-center align-items-center flex-column left-box"
-					style="background: rgb(59,125,247);
-                    background: linear-gradient(90deg, rgba(59,125,247,1) 0%, rgba(72,50,235,1) 35%, rgba(187,32,232,1) 100%);"
-				>
-					<div class="featured-image mb-3">
-						<img src={Portada} alt="" class="img-fluid" style="width: 250px;" loading="lazy" decoding="async" />
-					</div>
-					<p
-						class="text-white fs-2"
-						style="font-family: 'Courier New', Courier, monospace; font-weight: 600;"
-					>
-						Be Verified
-					</p>
-					<small
-						class="text-white text-wrap text-center"
-						style="width: 17rem;font-family: 'Courier New', Courier, monospace;"
-						>Join experienced Designers on this platform.</small
-					>
-				</div>
+					class="col-md-6 rounded-4 d-flex justify-content-center align-items-center flex-column left-box logImg"
+					style="background: URL({Portada}) no-repeat center center / cover;"
+				/>
 
 				<!-------------------- ------ Right Box ---------------------------->
 
 				<div class="col-md-6 right-box">
 					<div class="row align-items-center">
 						<div class="header-text mb-4">
-                            <div style="text-align: center;">
+							<div style="text-align: center;">
 								<img src={Policard} alt="" width="140px" loading="lazy" decoding="async" />
 							</div>
 							<h2 style="text-align: center; color: #000000;">Bienvenido a Policard 👋</h2>
-							<p class="fs-6 mb-4" style="color: #000000;">Inicie sesión para continuar con el trámite de tu credencial digital.</p>
+							<p class="fs-6 mb-4" style="color: #000000;">
+								Inicie sesión para continuar con el trámite de tu credencial digital.
+							</p>
 						</div>
 						<div>
 							<p style="color: #000000;">Ingresa tus credenciales</p>
@@ -139,5 +125,69 @@
 </body>
 
 <style>
-	@import "https://fonts.googleapis.com/css2?family=Poppins:wght@400;500&display=swap";body{background:url(../../lib/img/background1.jpg);background-position:center;background-repeat:no-repeat;background-size:cover;font-family:'Inter',sans-serif}.box-area{width:930px}.right-box{padding:40px 30px 40px 40px}::placeholder{font-size:16px}.rounded-4{border-radius:20px}.rounded-5{border-radius:30px}.invalid{color:#f63131}#innerPage{width:100%;max-width:840px;margin:0 auto;border-radius:12px;background:#f7fafc}.form-control{background:none;border:none;border-bottom:1px solid #a91ce5;color:#27272e}#imgBgn{background:url(../../lib/img/temo.jpg) no-repeat;background-size:cover;background-position:center;min-height:75vh;width:100%;border-radius:12px 0 0 12px}.btn{color:#fff;background-color:purple}@media only screen and (max-width: 767px){.box-area{margin:0 10px}.left-box{height:100px;overflow:hidden}.right-box{padding:20px}}
+	body {
+		background: url(../../lib/img/background1.jpg);
+		background-position: center;
+		background-repeat: no-repeat;
+		background-size: cover;
+		font-family: 'Inter', sans-serif;
+	}
+	.box-area {
+		width: 930px;
+	}
+	.right-box {
+		padding: 40px 30px 40px 40px;
+	}
+	::placeholder {
+		font-size: 16px;
+	}
+	.rounded-4 {
+		border-radius: 20px;
+	}
+	.rounded-5 {
+		border-radius: 30px;
+	}
+	.invalid {
+		color: #f63131;
+	}
+	#innerPage {
+		width: 100%;
+		max-width: 840px;
+		margin: 0 auto;
+		border-radius: 12px;
+		background: #f7fafc;
+	}
+	.form-control {
+		background: none;
+		border: none;
+		border-bottom: 1px solid #a91ce5;
+		color: #27272e;
+	}
+	#imgBgn {
+		background: url(../../lib/img/temo.jpg) no-repeat;
+		background-size: cover;
+		background-position: center;
+		min-height: 75vh;
+		width: 100%;
+		border-radius: 12px 0 0 12px;
+	}
+	.btn {
+		color: #fff;
+		background-color: purple;
+	}
+	@media only screen and (max-width: 767px) {
+		.box-area {
+			margin: 0 10px;
+		}
+		.left-box {
+			height: 100px;
+			overflow: hidden;
+		}
+		.right-box {
+			padding: 20px;
+		}
+
+	}
+
+
 </style>
